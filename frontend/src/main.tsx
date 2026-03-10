d21c2660023380d2fe816dae08e85d49bb6975e7
ShubhamBAdola <shubhambadola87@gmail.com>
2026-03-10 19:27:16 +0530

    feat(frontend): implement tailwind dark mode context and toggle

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,14 @@ import { AuthProvider } from 'react-oidc-context';
 import './index.css'
 import App from './App.tsx'
 import { authConfig } from './auth/authConfig.ts';
+import { ThemeProvider } from './context/ThemeContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider {...authConfig}>
-      <App />
-    </AuthProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <AuthProvider {...authConfig}>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
